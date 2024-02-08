060479ea2725c6b600f55a0dbddba0b6202ce32f
陳震庭 <chippo19970122@gmail.com>
2024-02-08 21:37:39 +0800

    back function

--- main.js
+++ main.js
@@ -8,37 +8,34 @@ const main = () => {
   const contact = document.querySelector(".contact")
   const generateBtn = document.querySelector(".generate-duck")
   const modal = document.querySelector(".show-story")
-  const next = document.querySelector(".step")
+  const next = document.querySelector(".next")
+  const back = document.querySelector(".prev")
 
   // utils
-  const toggleNext = () => {
-    if (next.classList.contains("hide")) {
-      next.classList.remove("hide")
-      return
-    }
-
-    next.classList.add("hide")
-  }
-
-  const toggleModal = () => {
-    if (modal.classList.contains("hide")) {
-      modal.classList.remove("hide")
-      return
-    }
-
-    modal.classList.add("hide")
-  }
-
   const showContact = (arr, idx) => {
     contact.innerHTML = arr[idx]
   }
+
   // intro
   let currentIntroStep = 0
 
+  const backIntro = () => {
+    currentIntroStep -= 1
+    showContact(Intro, currentIntroStep)
+
+    if (currentIntroStep < 1) {
+      back.classList.add("hide")
+      return
+    }
+
+    back.classList.remove("hide")
+  }
+
   const nextIntro = () => {
     if (currentIntroStep + 1 >= Intro.length) {
-      toggleNext()
-      toggleModal()
+      next.classList.add("hide")
+      back.classList.add("hide")
+      modal.classList.add("hide")
       field.start()
       animate()
       return
@@ -46,11 +43,17 @@ const main = () => {
 
     currentIntroStep += 1
     showContact(Intro, currentIntroStep)
+
+    if (currentIntroStep > 0) {
+      console.log("!!!")
+      back.classList.remove("hide")
+    }
   }
 
   contact.innerHTML = Intro[currentIntroStep]
 
   next.addEventListener("click", nextIntro)
+  back.addEventListener("click", backIntro)
 
   //game starts
   const CANVAS = document.getElementById("CANVAS")
@@ -60,22 +63,28 @@ const main = () => {
 
   const field = new Field(CANVAS)
 
-  generateBtn.addEventListener("click", () => {
-    if (field.isStart && !field.isFinish) {
-      field.generateField()
-      return
-    }
+  // generateBtn.addEventListener("click", () => {
+  //   if (field.isStart && !field.isFinish) {
+  //     field.generateField()
+  //     return
+  //   }
 
-    if (field.isStart && field.isFinish) {
-      field.start()
-      return
-    }
+  //   if (field.isStart && field.isFinish) {
+  //     field.start()
+  //     return
+  //   }
 
-    field.start()
-    animate()
-  })
+  //   field.start()
+  //   animate()
+  // })
 
   const animate = () => {
+    if (field.isStart && field.isFinish) {
+      //中斷點
+      console.log("DONE")
+      return
+    }
+
     field.draw()
     requestAnimationFrame(animate)
   }
--- readme.md
+++ readme.md
@@ -4,6 +4,8 @@
 
 https://chippo0122.github.io/tracking_duck_duck
 
+- Javascript CANVAS API 練習
+- Object Oriental Programing 練習
 - 隨機產生十隻鴨鴨
 - 要把鴨鴨全部抓回中間的巢裡
 
@@ -13,8 +15,8 @@ https://chippo0122.github.io/tracking_duck_duck
 - 鴨子要可以在畫布上亂跑 V
 - 建立 Control 實體 V
 - 鴨子在被抓住的時候不會亂跑，可以跟著手指自動拖曳 V
-- 若放開鴨子時，若不在巢的範圍內，鴨子應該繼續亂跑，在巢內則被抓起來
+- 若放開鴨子時，若不在巢的範圍內，鴨子應該繼續亂跑，在巢內則被抓起來 V
 - 全部抓到時，秀出文字
 - 添加動畫效果
 - 如果可以，添加音效
-- 開始按鈕
+- 開始按鈕 V
